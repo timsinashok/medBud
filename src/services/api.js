9239d66e85f5ebcbfaba471e8a9ef52a1ffc1df8
timsinashok <ashoktimsina147181@gmail.com>
2025-05-03 02:17:17 +0400

    backend url updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://medbud.onrender.com';
 
 export const api = {
   // Symptoms
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://localhost:8000/api/auth';
+const API_URL = 'https://medbud.onrender.com/api/auth';
 
 export const login = async (username, password) => {
   try {
